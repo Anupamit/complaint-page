e578f487043aaf2108d992d2ee814586a76ceed8
Anupam kumar <anupamit011@gmail.com>
2022-06-07 16:06:22 +0530

    add js for all components add together

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import './App.css';
-import Complaint from './Complaint';
+import Editcomplaint from './Editcomplaint';
+import Addcomplaint from './Addcomplaint';
+import Home from './Home';
+import ViewComplaint from './ViewComplaint';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="body">
-      <Complaint/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="editcomplaint" element={<Editcomplaint />} />
+        <Route path="addcomplaint" element={<Addcomplaint />} />
+        <Route path="viewcomplaints" element={<ViewComplaint />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 // complaint type
 // acedmics, manegent, bus, fee, class, sports, other
 // Date, con. person,discription
 // delete,add,edit   
+
 export default App;
