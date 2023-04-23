2cd647e51551b5ca2d1b51d43efe75e64b69c471
Anupam kumar <anupamit011@gmail.com>
2023-04-23 15:29:25 +0530

    add some modefied Appjs components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import Editcomplaint from './Editcomplaint';
-import Addcomplaint from './Addcomplaint';
-import Home from './Home';
-import ViewComplaint from './ViewComplaint';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home"
+import Navbar from "./Components/Navbar"
+import Editcomplaint from "./Components/Editcomplaint"
+import Addcomplaint from "./Components/Addcomplaint"
+import ViewComplaint from "./Components/ViewComplaint"
+
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="editcomplaint" element={<Editcomplaint />} />
-        <Route path="addcomplaint" element={<Addcomplaint />} />
-        <Route path="viewcomplaints" element={<ViewComplaint />} />
+        <Route path="/editcomplaint" element={<Editcomplaint />} />
+        <Route path="/addcomplaint" element={<Addcomplaint />} />
+        <Route path="/viewcomplaints" element={<ViewComplaint />} />
     </Routes>
   </BrowserRouter>
   );
